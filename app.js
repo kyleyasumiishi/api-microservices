05c26c5a1cdab7182dbbee8166a13e5e732b9be8
Kyle Yasumiishi <kyleyasumiishi@Ericas-MacBook-Pro.local>
2018-05-04 10:00:13 -0700

    Set up mongoose connection in app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const dev_db_url = 'mongodb://admin:admin@ds115360.mlab.com:15360/api-microservice'
+const mongoDB = process.env.MONGODB_URL || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
