67f0ab34a29d7bac1040e0582a6274f36c6aadb4
Kyle <k.yasumiishi@gmail.com>
2018-05-10 12:19:28 -0700

    Redefined userSchema, added salt for bcrypt-nodejs, modified pre-save action to hash password, and added npm dependencies for express-session, passport and passport-local

--- models/schemas/user.js
+++ models/schemas/user.js
@@ -1,43 +1,48 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
-const userSchema = new Schema({
-    username: {type: String, trim: true, required: true},
-    email: {type: String, unique: true, sparse: true},
-    hash: {type: String, required: true},
-    token: String
+const SALT_FACTOR = 10;
+
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, unique: true},
+    displayName: String
 });
 
 // Virtual for user's URL
 userSchema.virtual('url').get(function() {
-    return '/users/' + this._id;
+    return '/users/' + this.username;
 });
 
-// method for validating password
-userSchema.methods.comparePassword = function(pw, callback) {
+// Get user's name
+userSchema.methods.name = function() {
+    return this.displayName || this.username;
+};
+
+// Pre-save action to hash password
+userSchema.pre('save', function(done) {
+    let user = this;
+    if (!user.isModified('password')) {
+        return done();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) return done(err);
+        bcrypt.hash(user.password, salt, function(err, hashedPassword) {
+            if (err) return done(err);
+            this.password = hashedPassword;
+            done();
+        });
+    });
+});
+
+// Method for validating password
+userSchema.methods.cPassword = function(pw, callback) {
     bcrypt.compare(pw, this.hash, function(err, isMatch) {
         if (err) return callback(err);
         callback(null, isMatch);
     });
 };
-
-userSchema.pre('save', function(callback) {
-    if (!this.username) {
-        return callback(new Error('Missing username'));
-    }
-    if (!this.hash) {
-        return callback(new Error('Missing password'));
-    }
-    if (!this.email) {
-        return callback(new Error('Missing email'));
-    }
-    if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.email))) {
-        return callback(new Error('Invalid email'));
-    }
-    this.hash = bcrypt.hashSync(this.hash);
-    callback();
-})
 
 // Export model
 module.exports = mongoose.model('User', userSchema); 
